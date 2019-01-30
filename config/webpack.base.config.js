c29a9bc09689c44876c34883d1dc09498979ff0a
hwt <xiongjiehu@126.com>
2019-01-30 17:35:48 +0800

    webpack

---
+++ config/webpack.base.config.js
@@ -0,0 +1,46 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  devtool: "#source-map",
+  entry: ["./src"],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js",
+    publicPath: "/dist/"
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ["babel-loader"],
+        include: path.join(process.cwd(), "src")
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  }
+};
